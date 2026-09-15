c87c4841b83f0d6e055b851f9bf993a00d583eeb
18question <222290447+18question@users.noreply.github.com>
2026-09-16 00:46:55 +0800

    feat(simple): add `render_tree` and `render_subtree` to `ABCTreeRenderer`
    `render_tree` is the entry point; `render_subtree` performs the recursive traversal.

--- src/tree_renderer/simple/abc_tree_renderer.py
+++ src/tree_renderer/simple/abc_tree_renderer.py
@@ -24,3 +24,51 @@ class ABCTreeRenderer(ABC):
     @abstractmethod
     def on_node_exit(self, node, line_prefix):
         pass
+
+    def render_tree(self, root_node):
+        self.render_subtree(root_node, is_root=True, is_last=True)
+
+    def render_subtree(self, node, is_root, is_last):
+        # Process the line prefix.
+        line_prefix_list = self.indent_prefix_stack[:]
+        if not is_root:
+            if is_last:
+                # Push onto the stack.
+                self.indent_prefix_stack.append(self.glyph_style.indent_blank)
+                line_prefix_list.append(self.glyph_style.branch_end)
+            else:
+                # Push onto the stack.
+                self.indent_prefix_stack.append(self.glyph_style.indent_guide)
+                line_prefix_list.append(self.glyph_style.branch_mid)
+        line_prefix = "".join(line_prefix_list)
+
+        # Call the hook.
+        self.on_node_enter(node, line_prefix)
+
+        # Iterate child nodes.
+        child_iterator = iter(self.get_children(node))
+        try:
+            current_node = next(child_iterator)
+        except StopIteration:
+            # Length == 0
+            pass
+        else:
+            # Length >= 1
+            while True:
+                try:
+                    next_node = next(child_iterator)
+                except StopIteration:
+                    # Is last.
+                    self.render_subtree(current_node, is_root=False, is_last=True)
+                    break
+                else:
+                    # Is not last.
+                    self.render_subtree(current_node, is_root=False, is_last=False)
+                    current_node = next_node
+
+        # Call the hook.
+        self.on_node_exit(node, line_prefix)
+
+        # Pop from the stack.
+        if not is_root:
+            self.indent_prefix_stack.pop()
